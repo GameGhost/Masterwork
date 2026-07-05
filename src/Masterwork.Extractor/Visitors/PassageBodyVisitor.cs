3a6fa6a7a03af76d9f61ad3a77dd2193b08affa2
Duncan M <theghost@digitalghost.ca>
2026-07-04 21:15:51 -0600

    Code cleanup

--- src/Masterwork.Extractor/Visitors/PassageBodyVisitor.cs
+++ src/Masterwork.Extractor/Visitors/PassageBodyVisitor.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Text.RegularExpressions;
 using VarDef = Masterwork.ModuleFormat.VarDef;
 using Microsoft.CodeAnalysis;
